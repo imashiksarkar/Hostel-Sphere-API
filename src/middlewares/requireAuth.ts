6063fd4674e59a76fbdcc0ceef77b890ebbdbe0d
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2025-02-26 02:28:06 +0600

    - fix. passthrough auth

--- src/middlewares/requireAuth.ts
+++ src/middlewares/requireAuth.ts
@@ -27,14 +27,14 @@ const requireAuth = (passThrough = false) =>
       _res: Response,
       next: NextFunction
     ) => {
-      if (!req.headers?.authorization)
-        throw Err.setStatus('Unauthorized').setMessage('No token provided')
-
-      const token = req.headers.authorization.split(' ')[1]
-
       const user = {} as IUser
       let error = null
+
       try {
+        if (!req.headers?.authorization)
+          throw Err.setStatus('Unauthorized').setMessage('No token provided')
+
+        const token = req.headers.authorization.split(' ')[1]
         const { uid, name, email, picture, role, userId } = await verifyIdToken(
           token
         )
